c36bf72b2622d05171a9606402144f797324fde7
Lsickle <40272669+Lsickle@users.noreply.github.com>
2018-09-10 07:12:16 -0500

    Add files via upload

---
+++ housegamingphp/LolNews.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+     <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <script type="text/javascript" src="assets/js/all.js"></script>
+          <link rel="stylesheet" type="text/css" href="assets/css/news.css">
+          <title>noticia 1</title>
+     </head>
+     <body>
+          <article class="main">
+               <h1>League of Legends anuncia recompensas para sus creadores de contenidos</h1>
+               <section>
+                    <p>
+                         Hoy es un gran día para los creadores de contenidos seguidores del MOBA de Riot Games, y es que la poderosa compañía ha decidido premiar a aquellos que dan a conocer su contenido en League of Legends, que tanto han ayudado en el crecimiento de la comunidad y acercado los deportes electrónicos a todo tipo de público.
+                    </p>
+                    <br>
+                    <p>
+                         “Sabemos que League of Legends no sería nada sin la comunidad de creadores. El Programa de Colaboración de LoL surge de nuestra inquietud de reconocer el magnífico contenido que crean jugadores como tú, por brindarte nuestro apoyo para que sigas creciendo y por recompensar tu esfuerzo“, comunicaba la propia Riot Games.
+                    </p>
+                    <br>
+                    <p>
+                         Con este Programa de Colaboración de LoL, cualquier usuario que cumpla con los requisitos puede convertirse en un creador oficial de contenido, recibiendo recompensas exclusivas que van desde campeones y aspectos, chromas, regalos para sus espectadores, recibir invitaciones para eventos o incluso estar en contacto con los desarrolladores para de este modo poder anunciar en exclusiva los futuros contenidos, como son los eventos o nuevos campeones.
+                    </p>
+                    <br>
+                    <p>
+                         De modo que si ya eres un creador de contenido de League of Legends o estás pensando en dedicarte a ello, Riot te brinda la mano para ir codo con codo.
+                    </p>
+                    <br>
+               </section>
+               <pre>
+               <div class="video-responsive" data-layer="0">
+                    <iframe src="https://www.youtube.com/embed/pWnVv4mORRs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+               </div>
+               </pre>
+               <section class="redirect">
+                    <p>puedes saber mas en: <a href="https://lan.leagueoflegends.com/es/news/">https://lan.leagueoflegends.com/es/news/</a></p>
+               </section>
+          </article>
+     </body>
+</html>
