b1c05260412cb93153aa391d4394b163cbfe2e9a
majomaken <majo.maken@gmail.com>
2018-09-13 12:48:22 -0500

    Subiendo la foto de wow y colocando games en api para elegir

---
+++ housegamingphp/api/games.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Games</title>
+    <link rel="stylesheet" href="../assets/css/master.css">
+    <link rel="stylesheet" href="../assets/css/games.css">
+  </head>
+  <body>
+      <section>
+        <h2 id="palabra">Juegos en los que puedes crear tus equipos</h2>
+        <div class="container-games">
+
+            <a href="Lol.php"><img src="../assets/images/lolr.jpg" class="games"  alt="League Of Legends"></a>
+            <a href="Bliza.php"><img src="../assets/images/wow.jpg" class="games" alt="Wow"></a>>
+
+        </div>
+      </section>
+  </body>
+</html>
