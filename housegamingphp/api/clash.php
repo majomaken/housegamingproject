e7adedae6b46bfb7e5d5cf716b4373e241192a8a
xXFISHERXx <41491302+xXFISHERXx@users.noreply.github.com>
2018-09-21 13:46:00 -0500

    Add files via upload
    Api clash

---
+++ housegamingphp/api/clash.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Clash royale</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../assets/css/Api.css">
+</head>
+<body id="clash">
+	<form class="" action="royale.php" method="get">
+		<div id="kas">
+			<h1 for="informacion">Digita tu Tag<br><h2>(Sin el #)</h2></h1>
+			<img src="" class="cla">
+			<input id="roya" type="text" name="informacion" placeholder="Digita tu Tag" required="required">
+			<button id="cb" type="submit" name="button" onclick="location.href='https://royaleapi.com/player/LQYU9R89' ">Enviar</button>
+		</div>
+	</form>
+</body>
+</html>
