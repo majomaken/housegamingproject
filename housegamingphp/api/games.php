19d68e3f02e699636fd0d8f6419271d948bd8d08
majomaken <majo.maken@gmail.com>
2018-09-22 15:14:53 -0500

    Todo listo en api solucionado el error

--- housegamingphp/api/games.php
+++ housegamingphp/api/games.php
@@ -3,28 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>Games</title>
-<<<<<<< HEAD
-    <link href="https://fonts.googleapis.com/css?family=Notable|Noto+Serif+KR" rel="stylesheet"> 
-=======
->>>>>>> 3955bc0ab1b8106b5a4a3cada90d754f23f40cf5
+    <link href="https://fonts.googleapis.com/css?family=Notable|Noto+Serif+KR" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/master.css">
     <link rel="stylesheet" href="../assets/css/games.css">
   </head>
   <body>
       <section>
-<<<<<<< HEAD
         <h1 id="title-query">Compruebe las estadísticas de sus juegos</h1>
         <div class="container-games">
 
             <a href="Lol.php"><img src="../assets/images/lolr.jpg" class="games"  alt="League Of Legends"></a>
-=======
-        <h1 id="palabra">Consulta tus datos</h1>
-        <h2 id="palabra">Consulta los datos de tus MOBAS favoritos</h2>
-        <div class="container-games">
-
-            <a href="Lol.php"><img src="../assets/images/lolr.jpg" class="games"  alt="League Of Legends"></a>
-            <a href="Bliza.php"><img src="../assets/images/wow.jpg" class="games" alt="Wow"></a>>
->>>>>>> 3955bc0ab1b8106b5a4a3cada90d754f23f40cf5
             <a href="Star.php"><img src="../assets/images/star2.jpg" class="games" alt="Starcraft"></a>
             <a href="clash.php"><img src="../assets/images/clash.jpg" class="games" alt="Clash Royale"></a>
 
