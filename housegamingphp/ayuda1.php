853dba632d82101a24b8b5a15737b442603f7c23
Lsickle <40272669+Lsickle@users.noreply.github.com>
2018-09-25 02:22:12 -0500

    Add files via upload
    ayudas de crear equipo

---
+++ housegamingphp/ayuda1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+     <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <script type="text/javascript" src="assets/js/all.js"></script>
+          <link rel="stylesheet" type="text/css" href="assets/css/terminos.css">
+          <title>Como crear una equipo</title>
+     </head>
+     <body>
+          <video src="assets/video/background.mp4" autoplay loop></video>
+          <article style="color:#dfc7c7;">
+               <h1>Como crear una equipo</h1>
+               <ol type="1" class="numberlist">
+                    <li><img src="ayudasIMG/crearequipo/crearequipo1.png"></li>
+                    <li><img src="ayudasIMG/crearequipo/crearequipo2.png"></li>
+                    <li><img src="ayudasIMG/crearequipo/crearequipo3.png"></li>
+                    <li><img src="ayudasIMG/crearequipo/crearequipo4.png"></li>
+                    <li><img src="ayudasIMG/crearequipo/crearequipo5.png"></li>
+               </ol>
+          </article>
+          <section class="volversection">
+               <h1><a class="volverbutton" href="terminos.php">Condiciones de uso</a></h1>
+               <br>
+               <h3><a class="volverbutton" href="index.php">volver a la pagina principal</a></h3>
+
+          </section>
+
+
+     </body>
+</html>
